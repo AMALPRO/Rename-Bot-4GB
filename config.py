2cf590b2cd47af5cec6ec5dd3b5e5968a76378ce
AMALPRO <87407213+AMALPRO@users.noreply.github.com>
2024-06-26 21:21:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,19 @@
 import os
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", ""))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6426072097:AAFpvNEzW65qVT6XwTxzafDLJKFk-Xtbun0")
+API_ID = int(os.environ.get("API_ID", "6119632"))
+API_HASH = os.environ.get("API_HASH", "5b51582f938e93c8eab32612d5045737")
+OWNER = int(os.environ.get("OWNER", "1869495895"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "MikasaAckermansbot")
 
 FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002049486679"))
 
-DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://amal:amal@cluster0.n4bdwac.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DB_NAME", "rena")
 
-STRING = os.environ.get("STRING", "")
+STRING = os.environ.get("STRING", "1BVtsOJ0BuxU8Byssa_oQycEq_GuZzedIecoJeuolkotKQiDAETgZtqcn4a9b8caaeP-I2O2guVbw5YEPIuR7QJTAzz5NqJCF_MIG2L61ZMfJFKwDzTUGNdRcRsUECEvgrGKvDG7-PBOzd5IrfGyV2qRS5WFG-KQMz86BMjkSsjPkzg8IW4jbz_ltQuKUsZj2tMxxkbBlgik_DJcOHDGhWZSCj4Kiq4CTz1b0riNEjahBCxphxSmPOC5J6QcVjpj-SH5ArMcimxfvpLDjlM270ct1w66oU2O2zN1ITrnI3BJzlx8jA1c3NCiuV7jTg8KvOuatv6yT7-pynJXxaOQdRBRqEtg_Cx0=")
 BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/ad48ac09b1e6f30d2dae4.jpg")
 
 
